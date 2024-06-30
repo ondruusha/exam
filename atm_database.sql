381bd91050ee958105a0ff8123509ffd3445344b
Andrey <105154073+ondruusha@users.noreply.github.com>
2024-06-30 14:29:04 +0300

    7
    777

--- atm_database.sql
+++ atm_database.sql
@@ -1,14 +1,35 @@
-CREATE DATABASE IF NOT EXISTS BankATM;
-USE BankATM;
-
-CREATE TABLE ATMs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    bankName VARCHAR(100) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    status ENUM('Active', 'Inactive') NOT NULL
+-- Создание таблицы Банки
+CREATE TABLE Banks (
+    bank_code INT PRIMARY KEY,
+    bank_name VARCHAR(100),
+    legal_address VARCHAR(255)
 );
 
-INSERT INTO ATMs (bankName, location, status) VALUES
-('Bank A', 'City X', 'Active'),
-('Bank B', 'City Y', 'Inactive'),
-('Bank C', 'City Z', 'Active');
+-- Создание таблицы Банкоматы
+CREATE TABLE ATMs (
+    atm_number INT PRIMARY KEY,
+    address VARCHAR(255),
+    bank_code INT,
+    FOREIGN KEY (bank_code) REFERENCES Banks(bank_code)
+);
+
+-- Создание таблицы Клиенты
+CREATE TABLE Clients (
+    card_number VARCHAR(19) PRIMARY KEY,
+    full_name VARCHAR(100),
+    address VARCHAR(255),
+    bank_code INT,
+    FOREIGN KEY (bank_code) REFERENCES Banks(bank_code)
+);
+
+-- Создание таблицы Операции
+CREATE TABLE Transactions (
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    card_number VARCHAR(19),
+    atm_number INT,
+    date_time DATETIME,
+    commission BOOLEAN,
+    amount DECIMAL(10, 2),
+    FOREIGN KEY (card_number) REFERENCES Clients(card_number),
+    FOREIGN KEY (atm_number) REFERENCES ATMs(atm_number)
+);
