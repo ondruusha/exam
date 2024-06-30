3311b54ee5be30b265f3731002717f9080ada2c7
Andrey <105154073+ondruusha@users.noreply.github.com>
2024-06-30 18:53:17 +0300

    emd.
    end.

--- user.js
+++ user.js
@@ -6,13 +6,14 @@ function loadTransactions() {
     tableBody.innerHTML = '';
     transactions.forEach(transaction => {
         const row = document.createElement('tr');
+        const amountWithCommission = transaction.amount - (transaction.commission || 0);
         row.innerHTML = `
             <td>${transaction.transaction_id}</td>
             <td>${transaction.card_number}</td>
             <td>${transaction.atm_number}</td>
             <td>${transaction.date_time.replace('T', ' ')}</td>
             <td>${transaction.commission ? 'Да' : 'Нет'}</td>
-            <td>${transaction.amount}</td>
+            <td>${amountWithCommission}</td>
         `;
         tableBody.appendChild(row);
     });
